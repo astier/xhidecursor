0eaeee48aa52e0dc04525b0ac44164f4cb08f92c
r1w1s1 <ricardsonwilliams@gmail.com>
2026-08-21 00:17:08 -0300

    xhidecursor: ignore modifier key presses

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <X11/XKBlib.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/Xfixes.h>
 #include <X11/extensions/XInput2.h>
 
 #include <stdio.h>
@@ -18,29 +22,56 @@ void xi_select_events(const int event) {
 }
 
 int main(void) {
+    int fixes_event, fixes_error;
+    int xi_major = 2, xi_minor = 0;
+
     if (!(d = XOpenDisplay(NULL))) {
-        printf("Couldn't open Display.\n");
+        fprintf(stderr, "xhidecursor: couldn't open display %s\n",
+                XDisplayName(NULL));
         return 1;
     }
+    if (!XFixesQueryExtension(d, &fixes_event, &fixes_error)) {
+        fprintf(stderr, "xhidecursor: XFixes extension is unavailable\n");
+        XCloseDisplay(d);
+        return 1;
+    }
+    if (XIQueryVersion(d, &xi_major, &xi_minor) != Success) {
+        fprintf(stderr, "xhidecursor: XInput2 extension is unavailable\n");
+        XCloseDisplay(d);
+        return 1;
+    }
+
     r = XDefaultRootWindow(d);
     xi_select_events(XI_RawKeyPress);
     XEvent e;
-    XGenericEventCookie *c;
-    while (!XNextEvent(d, &e)) {
-        if (!XGetEventData(d, (c = &e.xcookie)))
+    while (XNextEvent(d, &e) == 0) {
+        if (e.type != GenericEvent ||
+            !XGetEventData(d, &e.xcookie))
             continue;
+
+        XGenericEventCookie *c = &e.xcookie;
         switch (c->evtype) {
-            case XI_RawKeyPress:
+            case XI_RawKeyPress: {
+                XIRawEvent *raw = (XIRawEvent *)c->data;
+                KeySym keysym = XkbKeycodeToKeysym(d, (KeyCode)raw->detail,
+                                                   0, 0);
+
+                if (IsModifierKey(keysym))
+                    break;
+
                 xi_select_events(XI_RawMotion);
                 XFixesHideCursor(d, r);
                 break;
+            }
             case XI_RawMotion:
                 xi_select_events(XI_RawKeyPress);
                 XFixesShowCursor(d, r);
                 break;
         }
         XFreeEventData(d, c);
+        /* Synchronize selection changes and discard stale events. */
         XSync(d, True);
     }
     XCloseDisplay(d);
+    return 0;
 }
